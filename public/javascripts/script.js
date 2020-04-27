b2551504b543bc1731dd32ed9297e66da6bc8348
Akhil Ajmera <AkhilAjmera@Akhils-MacBook-Pro.local>
2020-04-27 10:57:09 -0400

    CRUD 2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,3 +5,41 @@ $.ajax({
       console.log(data);
     }
 })
+
+$.ajax({
+  type:"get",
+  url:"http://127.0.0.1:3000/recipes"
+  success: function(data){
+    console.log("Successfull call to get");
+  }
+})
+
+$(document).ready(function(){
+  loadRecipeList();
+});
+
+function loadRecipeList(){
+  $.ajax({
+    url:"http://127.0.0.1:3000/recipes"
+    success: function(data){
+      var userRecipes = document.getElementById("userRecipes");
+      if (userRecipes){
+        while (userRecipes.firstChild) {
+          userRecipes.removeChild(userRecipes.firstChild);
+        }
+      }
+      for (var i=0;i<data.length;i++){
+        var recipeName = data[i].rname;
+        var prepTime = data[i].prep;
+        var cookTime = data[i].cook;
+        var rIngredients = data[i].ingedients;
+        var rInstructions = data[i].instructions;
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(recipeName+prepTime+cookTime+rIngedients+rInstructions));
+        userRecipes.appendChild(li);
+      }
+    },
+    error: function(data){
+      alert("error" + data.error)
+    }
+}
